fac5e6679f8ad45f13ed38381f1f2b353f4c861e
Serge-boro <serhiiborodin83@gmail.com>
2021-11-30 21:36:35 -0800

    add info

--- src/Rendering.js
+++ src/Rendering.js
@@ -17,7 +17,7 @@ const RenderTracks = ({ tracks, index }) =>
   ))
 
 const Wrapper = styled.div`
-  margin-top: 2rem;
+  margin-top: 4rem;
   @media screen and (max-width: 415px) {
     margin-top: 0.5rem;
   }
